dec623964ba1f46b1a8551ea7eaadfd4fd374d94
dwood29d <dwood29d@gmail.com>
2019-06-15 16:05:16 -0500

    Complete error handling exercises

---
+++ oop/error_handling_funtime/user_script.rb
@@ -0,0 +1,11 @@
+require_relative 'super_useful'
+
+puts "'five' == #{convert_to_int('five')}"
+
+feed_me_a_fruit
+
+one_punch_man = BestFriend.new('Saitama', 5, 'be the strongest')
+
+one_punch_man.talk_about_friendship
+one_punch_man.do_friendstuff
+one_punch_man.give_friendship_bracelet
